b77627dba6f08e4b9f6047d50fca31bde7742f8a
nabilkachlan <74494898+nabilkachlan@users.noreply.github.com>
2020-11-15 14:55:39 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Nabil Kachlan</title>
+ <meta name="description" content="Project Manager, Lead Developer and a fast learner, holder of many skills and a moto 'Break the boundaries set by others.'">
+</head>
+<body>
+	Hello World
+</body>
+</html>
